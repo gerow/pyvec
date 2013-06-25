da3bb25043913a7b7cb22508ef425f0841564654
gerow <gerow.mike@gmail.com>
2013-06-24 21:28:43 -0700

    added more operators for making it easier to use

--- vec2.py
+++ vec2.py
@@ -46,6 +46,10 @@ class Vec2:
       return 1
     return 0
 
+  def __iter__(self):
+    yield self.x
+    yield self.y
+
   def __add__(self, other):
     return Vec2(self.x + other.x, self.y + other.y)
 
@@ -61,6 +65,22 @@ class Vec2:
       return Vec2(self.x * other, self.y * other)
     return Vec2(self.x * other.x, self.y * other.y)
 
+  def __rmult__(self, other):
+    return self * other
+
+  def __neg__(self):
+    return Vec2(-self.x, -self.y)
+
+  def __invert__(self):
+    return -self
+
+  def __pos__(self):
+    return Vec2(self)
+
+  # don't implement __rdiv__ because deciding what
+  # to do is a little tricky and not really worth
+  # doing...
+
   def __div__(self, other):
     # Essentially the same as mult except we divide
     if (other.isinstance(other, numbers.Number)):
@@ -69,6 +89,10 @@ class Vec2:
 
   def dot(self, other):
     return self.x * other.x + self.y * other.y
+
+  def __pow__(self, other):
+    # overload the pow operation to do dot product
+    return self.dot(other)
 
   def unit(self):
     return Vec2(self.x / self.length, self.y / self.length)
